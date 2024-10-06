9b601f17674982c01f4bb9fe22a948df454a6ffa
ChurIQLab <churkin.vitaliy@mail.ru>
2024-10-06 19:40:47 +0300

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Churkin Vitaly on 06.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Churkin Vitaly on 06.10.2024.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
